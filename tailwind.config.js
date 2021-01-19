1fa19a0d4a57b80f2d4cce820ea1bb12c67bdb37
Alex Bowers <bowersbros@gmail.com>
2021-01-19 13:04:32 +0000

    Gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    future: {},
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
